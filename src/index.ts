4ddd58f54f8f947a9961eb8719c1ac220ca1649f
Soner Sayakci <s.sayakci@shopware.com>
2022-08-24 22:09:00 +0200

    feat: add shop scraper

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
+import { onSchedule } from './cron/schedule';
 import router from './router'
 
 addEventListener('fetch', (event) => {
   event.respondWith(router.handle(event.request))
 })
+
+addEventListener('scheduled', (event) => {
+  event.waitUntil(onSchedule())
+});
