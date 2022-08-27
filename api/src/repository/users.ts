a1fefd417a669d83c14431e18c9425aa4b90f5c3
Soner Sayakci <s.sayakci@shopware.com>
2022-08-27 20:22:10 +0200

    feat: delete all teams of user, when user will be deleted

--- api/src/repository/users.ts
+++ api/src/repository/users.ts
@@ -1,11 +1,12 @@
 import { getConnection } from "../db";
+import Teams from "./teams";
 
 export default class Users {
     static async delete(id: string): Promise<void> {
         const ownerTeams = await getConnection().execute(`SELECT id FROM team WHERE owner_id = ?`, [id]);
 
-        if (ownerTeams.rows.length > 0) {
-            throw new Error(`Cannot delete user that is the owner of a team. Delete all teams before.`);
+        for (let row of ownerTeams.rows) {
+            await Teams.deleteTeam(row.id);
         }
 
         await getConnection().execute(`DELETE FROM user WHERE id = ?`, [id]);
