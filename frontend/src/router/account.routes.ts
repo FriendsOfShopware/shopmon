130521a4217ad1036676cb2eda2dfbef9e7a39a0
Soner Sayakci <s.sayakci@shopware.com>
2022-08-28 00:16:22 +0200

    feat: load all routes using lazy load

--- frontend/src/router/account.routes.ts
+++ frontend/src/router/account.routes.ts
@@ -1,28 +1,19 @@
 import Layout from '@/views/auth/Layout.vue';
-import Login from '@/views/auth/Login.vue';
-import Register from '@/views/auth/Register.vue';
-import AccountConfirm from '@/views/auth/AccountConfirm.vue';
-import ForgotPassword from '@/views/auth/ForgotPassword.vue';
-import ForgotPasswordConfirm from '@/views/auth/ForgotPasswordConfirm.vue';
-
-import Settings from '@/views/account/Settings.vue';
-import ListShops from '@/views/account/ListShops.vue';
-import AddShop from '@/views/account/AddShop.vue';
 
 export default {
     path: '/account',
     component: Layout,
     children: [
         // without login
-        { name: 'account.login', path: 'login', component: Login },
-        { name: 'account.register', path: 'register', component: Register },
-        { name: 'account.confirm', path: 'confirm/:token', component: AccountConfirm },
-        { name: 'account.forgot.password', path: 'forgot-password', component: ForgotPassword },
-        { name: 'account.forgot.password.confirm', path: 'forgot-password/:token', component: ForgotPasswordConfirm },
+        { name: 'account.login', path: 'login', component: () => import('@/views/auth/Login.vue') },
+        { name: 'account.register', path: 'register', component: () => import('@/views/auth/Register.vue') },
+        { name: 'account.confirm', path: 'confirm/:token', component: () => import('@/views/auth/AccountConfirm.vue') },
+        { name: 'account.forgot.password', path: 'forgot-password', component: () => import('@/views/auth/ForgotPassword.vue') },
+        { name: 'account.forgot.password.confirm', path: 'forgot-password/:token', component: () => import('@/views/auth/ForgotPasswordConfirm.vue') },
 
         // with login
-        { name: 'account.settings', path: 'settings', component: Settings },
-        { name: 'account.shops.list', path: 'shops', component: ListShops},
-        { name: 'account.shops.new', path: 'shops/new', component: AddShop},
+        { name: 'account.settings', path: 'settings', component: () => import('@/views/account/Settings.vue') },
+        { name: 'account.shops.list', path: 'shops', component: () => import('@/views/account/ListShops.vue') },
+        { name: 'account.shops.new', path: 'shops/new', component: () => import('@/views/account/AddShop.vue') },
     ]
 };
