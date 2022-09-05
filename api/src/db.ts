05ce136bab45dda5582dca087d22889e48bda666
Soner Sayakci <s.sayakci@shopware.com>
2022-09-05 18:24:16 +0200

    feat: convert datetime values correct

--- api/src/db.ts
+++ api/src/db.ts
@@ -1,9 +1,20 @@
-import { connect, Connection } from '@planetscale/database'
+import { connect, Connection, cast, Field } from '@planetscale/database'
 
-let workerConn: Connection|null = null;
+function inflate(field: Field, value: any) {
+  if (field.type === 'DATETIME') {
+    if (value === null) {
+      return null
+    }
+
+    return new Date(value).toISOString()
+  }
+  return cast(field, value)
+}
+
 
 export function getConnection(env: Env): Connection {
   return connect({
+    cast: inflate,
     host: env.DATABASE_HOST,
     username: env.DATABASE_USER,
     password: env.DATABASE_PASSWORD,
