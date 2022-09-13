fa38f55dd5415335dcf9eaf5ba18cbc24db4be56
Shyim <6224096+shyim@users.noreply.github.com>
2022-09-13 11:57:37 +0200

    chore: update db.sql

--- db.sql
+++ db.sql
@@ -2,11 +2,14 @@ CREATE TABLE `shop` (
 	`id` int NOT NULL AUTO_INCREMENT,
 	`team_id` int NOT NULL,
 	`name` varchar(255) NOT NULL,
+	`status` varchar(255) NOT NULL DEFAULT 'green',
 	`url` varchar(255) NOT NULL,
+	`favicon` varchar(500),
 	`client_id` varchar(255) NOT NULL,
 	`client_secret` varchar(255) NOT NULL,
 	`shopware_version` varchar(255),
 	`last_scraped_at` datetime,
+	`last_scraped_error` text,
 	`created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),
 	`updated_at` datetime,
 	PRIMARY KEY (`id`),
@@ -14,19 +17,33 @@ CREATE TABLE `shop` (
 ) ENGINE InnoDB,
   CHARSET utf8mb4,
   COLLATE utf8mb4_0900_ai_ci;
-
+  
+  CREATE TABLE `shop_pagespeed` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`shop_id` int NOT NULL DEFAULT '0',
+	`created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+	`performance` tinyint NOT NULL DEFAULT '0',
+	`accessibility` tinyint NOT NULL DEFAULT '0',
+	`bestpractices` tinyint NOT NULL DEFAULT '0',
+	`seo` tinyint NOT NULL DEFAULT '0',
+	PRIMARY KEY (`id`)
+) ENGINE InnoDB,
+  CHARSET utf8mb4,
+  COLLATE utf8mb4_0900_ai_ci;
+  
   CREATE TABLE `shop_scrape_info` (
 	`shop_id` int NOT NULL,
 	`extensions` text NOT NULL,
 	`scheduled_task` text NOT NULL,
-	`queue_info` text,
-	`cache_info` text,
+	`queue_info` text NOT NULL,
+	`cache_info` text NOT NULL,
+	`checks` text,
 	`created_at` datetime NOT NULL,
 	PRIMARY KEY (`shop_id`)
 ) ENGINE InnoDB,
   CHARSET utf8mb4,
   COLLATE utf8mb4_0900_ai_ci;
-
+  
   CREATE TABLE `team` (
 	`id` int NOT NULL AUTO_INCREMENT,
 	`name` varchar(255),
@@ -38,7 +55,7 @@ CREATE TABLE `shop` (
 ) ENGINE InnoDB,
   CHARSET utf8mb4,
   COLLATE utf8mb4_0900_ai_ci;
-
+  
   CREATE TABLE `user` (
 	`id` int NOT NULL AUTO_INCREMENT,
 	`username` varchar(50),
@@ -53,7 +70,23 @@ CREATE TABLE `shop` (
 ) ENGINE InnoDB,
   CHARSET utf8mb4,
   COLLATE utf8mb4_0900_ai_ci;
-
+  
+  CREATE TABLE `user_notification` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`user_id` int NOT NULL,
+	`key` varchar(50) NOT NULL,
+	`level` varchar(10) NOT NULL,
+	`title` varchar(128) NOT NULL DEFAULT '',
+	`message` varchar(128) NOT NULL,
+	`link` varchar(128) NOT NULL,
+	`read` tinyint NOT NULL DEFAULT '0',
+	`created_at` datetime DEFAULT CURRENT_TIMESTAMP(),
+	PRIMARY KEY (`id`),
+	UNIQUE KEY `unique_notification` (`user_id`, `key`)
+) ENGINE InnoDB,
+  CHARSET utf8mb4,
+  COLLATE utf8mb4_0900_ai_ci;
+  
   CREATE TABLE `user_to_team` (
 	`user_id` int NOT NULL,
 	`team_id` int NOT NULL,
