9449ce1dbc2cd28e7edd1c64219719791aae9cf2
pumpi <sf@pumpi-online.de>
2022-09-17 13:48:12 +0200

    fix: account menu no shadow

--- frontend/src/components/layout/Nav.vue
+++ frontend/src/components/layout/Nav.vue
@@ -148,7 +148,7 @@ function toggleDarkMode() {
               enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100"
               leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100"
               leave-to-class="transform opacity-0 scale-95">
-              <MenuItems class="origin-top-right absolute right-0 mt-2 w-48 rounded-md py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-10 dark:bg-neutral-800">
+              <MenuItems class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white focus:outline-none z-10 dark:bg-neutral-800">
                 <div class="px-4 py-2 border-b dark:border-neutral-700">
                   <div class="text-base font-medium">
                     {{ authStore.user.username }}
