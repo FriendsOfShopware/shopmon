c07cedf66ea6efeb17b528049e22680ec711373e
pumpi <sf@pumpi-online.de>
2023-05-10 07:41:51 +0000

    fix: rating component warning on null

--- frontend/src/components/layout/RatingStars.vue
+++ frontend/src/components/layout/RatingStars.vue
@@ -1,13 +1,13 @@
 <script lang="ts" setup>
-    defineProps<{rating: number}>()
+    defineProps<{rating: number | null}>()
 </script>
 
 <template>
-    <span :data-tooltip="`${rating / 2} from 5`" class="tooltip-position-center">
-        <template v-if="rating !== null" v-for="n in 5">
-        <icon-fa6-regular:star v-if="(rating / 2) - n < -.5" />
-        <icon-fa6-solid:star-half-stroke v-else-if="(rating / 2) - n === -.5" />
-        <icon-fa6-solid:star v-else />
-    </template>
+    <span :data-tooltip="`${rating / 2} from 5`" class="tooltip-position-center" v-if="rating !== null">
+        <template v-for="n in 5">
+            <icon-fa6-regular:star v-if="(rating / 2) - n < -.5" />
+            <icon-fa6-solid:star-half-stroke v-else-if="(rating / 2) - n === -.5" />
+            <icon-fa6-solid:star v-else />
+        </template>
     </span>    
 </template>
