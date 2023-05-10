842273b53e111b5ec70682b7334685373808097c
pumpi <sf@pumpi-online.de>
2023-05-10 10:07:33 +0200

    feat: DataTable search for ListExtensions (#141)

--- frontend/src/components/layout/DataTable.vue
+++ frontend/src/components/layout/DataTable.vue
@@ -1,12 +1,12 @@
  <template>
-    <table class="min-w-full divide-y-2 divide-gray-300 dark:divide-neutral-900" v-if="data.length > 0">
+    <table class="min-w-full divide-y-2 divide-gray-300 dark:divide-neutral-900" v-if="sortedFilteredData.length > 0">
         <thead class="bg-gray-50 dark:bg-neutral-800">
             <tr>
                 <th v-for="(label, key, index) in labels" 
                     :key="key" 
                     class="py-3.5"
                     :class="[
-                        {'px-3 text-left': !label.class && !label.classOverride},
+                        {'px-3 text-left whitespace-nowrap': !label.class && !label.classOverride},
                         {'pl-4 lg:pl-8': index === 0 && !label.classOverride},
                         {'pr-4 sm:pr-6 lg:pr-8': index === Object.keys(labels).length - 1 && !label.classOverride},
                         {'cursor-pointer': label.sortable},
@@ -25,7 +25,7 @@
             </tr>
         </thead>
         <tbody class="divide-y divide-gray-200 dark:divide-none">
-            <tr class="group even:bg-gray-50 hover:bg-sky-50 dark:even:bg-[#2b2b2b] dark:hover:bg-[#2a2b2f]" v-for="(item, itemKey) in sortedData" :key="itemKey">
+            <tr class="group even:bg-gray-50 hover:bg-sky-50 dark:even:bg-[#2b2b2b] dark:hover:bg-[#2a2b2f]" v-for="(item, itemKey) in sortedFilteredData" :key="itemKey">
                 <td v-for="(label, key, index) in labels"
                     class="whitespace-nowrap py-4 align-middle"
                     :class="[
@@ -36,7 +36,7 @@
                     ]"
                     :key="key"
                 >
-                    <slot :name="`cell(${key})`" :value="item[key]" :item="item" :data="sortedData" :itemKey="itemKey">
+                    <slot :name="`cell(${key})`" :value="item[key]" :item="item" :data="sortedFilteredData" :itemKey="itemKey">
                         {{ item[key] }}
                     </slot>
                 </td>
@@ -44,34 +44,46 @@
         </tbody>
     </table>
     <div class="p-6 text-center text-gray-400" v-else>
-        <icon-fa6-solid:inbox class="text-9xl text-gray-200 mb-4" />
-        <h2 class="text-2xl font-bold text-gray-400 block">No Data</h2>
-        There is no data to display the table
+        <template v-if="sortedFilteredData.length === 0">
+            <span class="text-lg">
+                <icon-fa6-solid:circle-xmark /> no search result for <strong>{{ term }}</strong>
+            </span>            
+        </template>
+        <template v-else>
+            <icon-fa6-solid:inbox class="text-9xl text-gray-200 mb-4" />
+            <h2 class="text-2xl font-bold text-gray-400 block">No Data</h2>
+            There is no data to display the table
+        </template>        
     </div>
 </template>
 
 <script lang="ts" setup>
-    import { defineProps, ref, computed } from 'vue'
+    import { defineProps, ref, watch, toRef } from 'vue'
     import { createNewSortInstance } from 'fast-sort';
+    import Fuse from 'fuse.js'
 
-    const props = defineProps<{labels: Record<string, {class?: string, classOverride?: boolean, name: string, sortable?: boolean}>, defaultSorting?: {by: string, desc?: boolean},data: Record<string, any>[]}>()
+    const props = defineProps<{
+        labels: Record<string, {
+            class?: string, 
+            classOverride?: boolean, 
+            name: string, 
+            sortable?: boolean
+        }>, 
+        defaultSorting?: {
+            by: string,
+            desc?: boolean
+        },
+        term?: string,
+        data: Record<string, any>[]
+    }>()
 
+    const term = toRef(props, 'term');
     const sortBy = ref(props.defaultSorting?.by || null);
     const sortDesc = ref(props.defaultSorting?.desc || null);
+    const sortedFilteredData = ref(props.data);       
 
     const naturalSort = createNewSortInstance({
         comparer: new Intl.Collator(undefined, { numeric: true, sensitivity: 'base' }).compare,
-    });
-
-    const sortedData = computed(() => {
-        const { data } = props;
-        if (sortBy.value === null) return data;
-
-        if (sortDesc.value) {
-            return naturalSort(data).desc(sortBy.value);
-        } else {
-            return naturalSort(data).asc(sortBy.value);
-        }
     });
 
     const setSort = (key: string) => {
@@ -86,4 +98,25 @@
             sortDesc.value = null;
         }
     };
+
+    const filterData = (data: Record<string, any>[], term: string | undefined) => {
+        if (term && term.length >= 3) {
+            const fuse = new Fuse(data, {
+                keys: ['label', 'name'],
+                threshold: 0,
+            });
+            return fuse.search(term).map(result => result.item);
+        }
+        return data;
+    }
+
+    watch([term, sortBy, sortDesc], ([newTerm, newSortBy, newSortDesc], [oldTerm, oldSortBy, oldSortDesc]) => {        
+        let filteredData = filterData(props.data, newTerm);
+
+        if (newSortBy !== null) {
+            filteredData = newSortDesc ? naturalSort(filteredData).desc(newSortBy) : naturalSort(filteredData).asc(newSortBy);
+        }
+
+        sortedFilteredData.value = filteredData;
+    }, { immediate: true });
 </script>
--- frontend/src/views/account/ListExtensions.vue
+++ frontend/src/views/account/ListExtensions.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import Header from '@/components/layout/Header.vue';
 import MainContainer from '@/components/layout/MainContainer.vue';
 import DataTable from '@/components/layout/DataTable.vue';
@@ -6,6 +7,8 @@ import DataTable from '@/components/layout/DataTable.vue';
 import { useExtensionStore } from '@/stores/extension.store';
 
 const extensionStore = useExtensionStore();
+
+const term = ref('');
 
 extensionStore.loadExtensions();
 
@@ -32,66 +35,70 @@ extensionStore.loadExtensions();
       </div>
     </div>
 
-    <div
-      class="shadow rounded-md overflow-y-scroll md:overflow-y-hidden bg-white dark:bg-neutral-800 dark:shadow-none"
-      v-else>
-      <DataTable
-          :labels="{label: {name: 'Name', sortable: true}, shop: {name: 'Shop'}, version: {name: 'Version'}, latestVersion: {name: 'Latest'}, ratingAverage: {name: 'Rating', sortable: true}, issue: {name: 'Known Issue', class: 'px-3 text-right'}}"
-          :data="extensionStore.extensions"
-          :default-sorting="{by: 'label'}">
-          <template #cell(label)="{ item }">            
-            <div v-if="item.storeLink">
-              <a :href="item.storeLink" target="_blank">
+    <template v-else>
+      <input v-model="term" class="field field-search mb-3" placeholder="Search ..." />
+
+      <div class="shadow rounded-md overflow-y-scroll md:overflow-y-hidden bg-white dark:bg-neutral-800 dark:shadow-none">
+        <DataTable
+            :labels="{label: {name: 'Name', sortable: true}, shop: {name: 'Shop'}, version: {name: 'Version'}, latestVersion: {name: 'Latest'}, ratingAverage: {name: 'Rating', sortable: true}, issue: {name: 'Known Issue', class: 'px-3 text-right'}}"
+            :data="extensionStore.extensions"
+            :default-sorting="{by: 'label'}"
+            :term="term">
+            <template #cell(label)="{ item }">            
+              <div v-if="item.storeLink">
+                <a :href="item.storeLink" target="_blank">
+                  <div class="font-bold whitespace-normal" v-if="item.label">{{ item.label }}</div>
+                  <div class="dark:opacity-50">{{ item.name }}</div>
+                </a>
+              </div>
+              <div v-else>
                 <div class="font-bold whitespace-normal" v-if="item.label">{{ item.label }}</div>
                 <div class="dark:opacity-50">{{ item.name }}</div>
-              </a>
-            </div>
-            <div v-else>
-              <div class="font-bold whitespace-normal" v-if="item.label">{{ item.label }}</div>
-              <div class="dark:opacity-50">{{ item.name }}</div>
-            </div>
-          </template>
-
-          <template #cell(shop)="{ item }">
-            <div class="mb-1" v-for="shop in item.shops">
-              <router-link :to="{ name: 'account.shops.detail', params: { teamId: shop.team_id, shopId: shop.id } }">
-                <span class="leading-5 text-gray-400 mr-1 text-base dark:text-neutral-500" data-tooltip="Not installed" v-if="!shop.installed">
-                  <icon-fa6-regular:circle />
-                </span>
-                <span class="leading-5 text-green-400 mr-1 text-base dark:text-green-300" data-tooltip="Active" v-else-if="shop.active">
-                  <icon-fa6-solid:circle-check />
-                </span>
-                <span class="leading-5 text-gray-300 mr-1 text-base dark:text-neutral-500" data-tooltip="Inactive" v-else>
-                  <icon-fa6-solid:circle-xmark />
-                </span>
-                {{ shop.name }}
-              </router-link>
-            </div>
-          </template>
-
-          <template #cell(version)="{ item }">
-            <div class="mb-1" v-for="shop in item.shops">
-              {{ shop.version }}
-              <span v-if="item.latestVersion && shop.version < item.latestVersion" data-tooltip="Update available">
-                <icon-fa6-solid:rotate class="ml-1 text-base text-amber-600 dark:text-amber-400" />
-              </span>
-            </div>
-          </template>
-
-          <template #cell(ratingAverage)="{ item }">
-            <RatingStars :rating="item.ratingAverage" />
-          </template>
-
-          <template #cell(installedAt)="{ item }">
-            <template v-if="item.installedAt">
-              {{ new Date(item.installedAt).toLocaleString() }}
+              </div>
             </template>
-          </template>
 
-          <template #cell(issue)="{ item }">
-            No known issues. <a href="#">Report issue</a>
-          </template>
-        </DataTable>
-    </div>
+            <template #cell(shop)="{ item }">
+              <div class="mb-1" v-for="shop in item.shops">
+                <router-link :to="{ name: 'account.shops.detail', params: { teamId: shop.team_id, shopId: shop.id } }">
+                  <span class="leading-5 text-gray-400 mr-1 text-base dark:text-neutral-500" data-tooltip="Not installed" v-if="!shop.installed">
+                    <icon-fa6-regular:circle />
+                  </span>
+                  <span class="leading-5 text-green-400 mr-1 text-base dark:text-green-300" data-tooltip="Active" v-else-if="shop.active">
+                    <icon-fa6-solid:circle-check />
+                  </span>
+                  <span class="leading-5 text-gray-300 mr-1 text-base dark:text-neutral-500" data-tooltip="Inactive" v-else>
+                    <icon-fa6-solid:circle-xmark />
+                  </span>
+                  {{ shop.name }}
+                </router-link>
+              </div>
+            </template>
+
+            <template #cell(version)="{ item }">
+              <div class="mb-1" v-for="shop in item.shops">
+                {{ shop.version }}
+                <span v-if="item.latestVersion && shop.version < item.latestVersion" data-tooltip="Update available">
+                  <icon-fa6-solid:rotate class="ml-1 text-base text-amber-600 dark:text-amber-400" />
+                </span>
+              </div>
+            </template>
+
+            <template #cell(ratingAverage)="{ item }">
+              <RatingStars :rating="item.ratingAverage" />
+            </template>
+
+            <template #cell(installedAt)="{ item }">
+              <template v-if="item.installedAt">
+                {{ new Date(item.installedAt).toLocaleString() }}
+              </template>
+            </template>
+
+            <template #cell(issue)="{ item }">
+              No known issues. <a href="#">Report issue</a>
+            </template>
+          </DataTable>
+      </div>
+    </template>
+    
   </MainContainer>
 </template>
