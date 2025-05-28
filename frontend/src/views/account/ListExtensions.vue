de559d5252d9418dc2a0d20a9496d40b3f7cec22
Wanne Van Camp <3399877+wannevancamp@users.noreply.github.com>
2025-05-28 06:18:12 +0200

    Fix routes (#436)

--- frontend/src/views/account/ListExtensions.vue
+++ frontend/src/views/account/ListExtensions.vue
@@ -3,7 +3,7 @@
 
     <main-container v-if="!extensionStore.isLoading">
         <template v-if="extensionStore.extensions.length === 0">
-            <element-empty title="No Apps" button="Add Shop" route="/account/shop/new">
+            <element-empty title="No Apps" button="Add Shop" route="/account/shops/new">
                 Get started by adding your first Shop.
             </element-empty>
         </template>
