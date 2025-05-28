a874e3fa40bfd817fd2b045002ff67f8086b78ff
Soner Sayakci <s.sayakci@gmail.com>
2025-05-28 13:19:15 +0200

    fix: set Sentry tracesSampleRate to 1.0

--- api/src/index.ts
+++ api/src/index.ts
@@ -17,6 +17,7 @@ Sentry.init({
     dsn: process.env.SENTRY_DSN || '',
     release: process.env.SENTRY_RELEASE || 'unknown',
     environment: process.env.SENTRY_ENVIRONMENT || 'development',
+    tracesSampleRate: 0.1,
 });
 
 const app = new Hono();
