7ceae32812934909a059ecfc8fa3061bacda7d4b
Soner Sayakci <s.sayakci@gmail.com>
2025-05-29 15:41:37 +0200

    refactor: add experimental logging configuration to Sentry initialization

--- api/src/sentry.ts
+++ api/src/sentry.ts
@@ -5,4 +5,7 @@ Sentry.init({
     environment: process.env.SENTRY_ENVIRONMENT || 'development',
     tracesSampleRate: 0.1,
     integrations: [Sentry.nativeNodeFetchIntegration()],
+    _experiments: {
+        enableLogs: true,
+    }
 });
