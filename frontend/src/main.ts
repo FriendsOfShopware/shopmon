5eb65155e70287fce691befbad13bd8d290ac719
Soner Sayakci <s.sayakci@gmail.com>
2025-05-30 21:42:11 +0200

    refactor: remove pinia dependency from package.json and main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,3 @@
-import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 
 import './style.css';
@@ -8,7 +7,6 @@ import { router } from './router';
 
 const app = createApp(App);
 
-app.use(createPinia());
 app.use(router);
 
 app.mount('#app');
