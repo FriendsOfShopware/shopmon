fee73b120dfa7f78609ba6587933285f60c989ad
Soner Sayakci <s.sayakci@gmail.com>
2025-06-19 18:00:45 +0200

    refactor: remove unused randomString function from util.ts

--- api/src/util.ts
+++ api/src/util.ts
@@ -1,16 +1,3 @@
-export function randomString(length: number): string {
-    let result = '';
-    const characters =
-        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const charactersLength = characters.length;
-    for (let i = 0; i < length; i++) {
-        result += characters.charAt(
-            Math.floor(Math.random() * charactersLength),
-        );
-    }
-    return result;
-}
-
 export type Version = string | number;
 
 /**
