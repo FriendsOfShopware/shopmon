5de293bf5686ade5ebdc7e220e781297548c5a66
Soner Sayakci <s.sayakci@gmail.com>
2026-03-18 12:24:49 +0100

    refactor: modal to use native html element

--- frontend/src/test-setup.ts
+++ frontend/src/test-setup.ts
@@ -1,5 +1,20 @@
 import { config } from "@vue/test-utils";
 
+// jsdom does not implement HTMLDialogElement.showModal / .close
+if (typeof HTMLDialogElement !== "undefined") {
+  HTMLDialogElement.prototype.showModal ??= function () {
+    this.setAttribute("open", "");
+  };
+  HTMLDialogElement.prototype.close ??= function () {
+    this.removeAttribute("open");
+  };
+} else {
+  // Fallback when jsdom exposes <dialog> as a generic HTMLElement
+  HTMLElement.prototype.showModal ??= function () {
+    this.setAttribute("open", "");
+  };
+}
+
 // Globally stub router-link to prevent Vue warnings
 config.global.stubs = {
   "router-link": {
